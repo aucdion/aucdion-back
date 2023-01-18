86c7cddd8c138c457de4a0980d1333977defd4b1
Egg <jj.lee@pet-friends.co.kr>
2023-01-18 22:30:38 +0900

    feat: add upsert

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { axiosInstance } from 'src/utils/axios';
+import { Item } from './dto/item.output';
 import { SearchInput } from './dto/search.dto';
 
 @Injectable()
@@ -17,11 +18,21 @@ export class ItemsService {
 
   async findAllByKeyword(searchInput: SearchInput) {
     try {
-      const { data } = await axiosInstance.get('/items', {
+      const { data } = await axiosInstance.get<{ rows: Item[] }>('/items', {
         params: {
           ...searchInput,
           wordType: 'full',
         },
+      });
+
+      data.rows.map((item) => {
+        this.prisma.item.upsert({
+          where: {
+            itemId: item.itemId,
+          },
+          create: item,
+          update: {},
+        });
       });
 
       return data.rows;
